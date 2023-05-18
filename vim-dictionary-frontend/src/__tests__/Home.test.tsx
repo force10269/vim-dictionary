23f659f3c11f4d26d2777c788368dd1200b03fc8
force10269 <korry.tunnicliff@colorado.edu>
2023-05-18 17:50:54 -0600

    test: 3 more unit tests for index page

--- vim-dictionary-frontend/src/__tests__/Home.test.tsx
+++ vim-dictionary-frontend/src/__tests__/Home.test.tsx
@@ -31,4 +31,34 @@ describe("Home", () => {
     fireEvent.change(globalSearchInput, { target: { value: "delete" } });
     expect(globalSearchInput).toHaveValue("delete");
   });
+
+  test("renders clear button and clears search input", () => {
+    const clearButton = screen.getByText("Clear");
+    expect(clearButton).toBeInTheDocument();
+
+    const searchInput = screen.getByPlaceholderText("Type keymap (e.g. dd)");
+    fireEvent.keyDown(searchInput, { key: "a" });
+    expect(searchInput).toHaveValue("a");
+
+    fireEvent.click(clearButton);
+    expect(searchInput).toHaveValue("");
+  });
+
+  test("renders github link and validates url", () => {
+    const githubLink = screen.getByRole("link");
+    expect(githubLink).toBeInTheDocument();
+    expect(githubLink.getAttribute("href")).toBe(
+      "https://github.com/force10269/vim-dictionary"
+    );
+  });
+
+  test("filters key mappings", () => {
+    const rowsBeforeFilter = screen.getAllByRole("row");
+
+    const searchInput = screen.getByPlaceholderText("Type keymap (e.g. dd)");
+    fireEvent.keyDown(searchInput, { key: "a" });
+
+    const rowsAfterFilter = screen.getAllByRole("row");
+    expect(rowsAfterFilter.length).toBeLessThan(rowsBeforeFilter.length);
+  });
 });
